b6968400bab9de5021cd309a2830a99758599d0d
Viral.n.patel <viral.n.patel@1rivet.com>
2023-08-31 13:28:15 +0530

    fix : add project and project status model

--- src/comman/model.ts
+++ src/comman/model.ts
@@ -1,10 +1,24 @@
 export interface Draggable {
-    dragStartHandler(event: DragEvent,projectId:string): void;
-    dragEndHandler(event: DragEvent): void;
+    dragStartHandler(event: DragEvent, projectId: string): void;
 }
 
 export interface DragTarget {
     dragOverHandler(event: DragEvent): void;
     dropHandler(event: DragEvent): void;
     dragLeaveHandler(event: DragEvent): void;
+}
+
+export enum ProjectStatus {
+    Active,
+    Finished
+}
+
+export class Project {
+    constructor(public id: string,
+        public title: string,
+        public description: string,
+        public people: string | number,
+        public status: ProjectStatus) {
+
+    }
 }
